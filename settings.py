790dcc8830e03d2a73c511df58c0b90bf445360a
Arpita Jain <jainarpita0806@gmail.com>
2026-07-21 16:04:21 +0530

    first commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_clr = (70, 130, 180)
+        self.ship_speed = 2.3
+        self.bullet_speed = 2.0
+        self.bullet_width = 4
+        self.bullet_height = 15
+        self.bullet_clr = (60,60,60)
+        
